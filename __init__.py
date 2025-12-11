cc44a42a9a299945bee62b791118b9e61f835ff2
LQT <hefiliyeyus@gmail.com>
2025-12-11 17:06:41 +0800

    新增两个翻译器

--- __init__.py
+++ __init__.py
@@ -1,11 +1,15 @@
-from .node import RN_Translator_Node
+from .node import RN_Translator_Node, RN_Prompt_Translator, RN_SeedXPro_Translator
 
 NODE_CLASS_MAPPINGS = {
     "RN_Translator_Node": RN_Translator_Node,
+    "RN Prompt Translator": RN_Prompt_Translator,
+    "RN SeedXPro Translator": RN_SeedXPro_Translator,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "RN_Translator_Node": "RN 翻译器",
+    "RN_Translator_Node": "RunNode 翻译器",
+    "RN Prompt Translator": "RunNode Translater",
+    "RN SeedXPro Translator": "RunNode SeedXPro Translater",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

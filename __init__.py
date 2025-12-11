483708d71ba445effc7c49e51ae1e6fb7913fbb3
LQT <hefiliyeyus@gmail.com>
2025-12-11 15:44:17 +0800

    Revert "新增seedxpro、prompter"
    This reverts commit 101f792d5b78b07bdcd2636da007efd8b2592c90.

--- __init__.py
+++ __init__.py
@@ -1,17 +1,11 @@
 from .node import RN_Translator_Node
-from .seedxpro_nodes import RN_SeedXPro_Translator
-from .prompter import RN_Prompt_Translator
 
 NODE_CLASS_MAPPINGS = {
     "RN_Translator_Node": RN_Translator_Node,
-    "RN SeedXPro Translator": RN_SeedXPro_Translator,
-    "RN Prompt Translator": RN_Prompt_Translator,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RN_Translator_Node": "RN 翻译器",
-    "RN SeedXPro Translator": "RN SeedXPro 翻译器",
-    "RN Prompt Translator": "RN Prompt 翻译器",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']

6ffad02682f9e0250f265eebb28570affddb66b0
LQT <hefiliyeyus@gmail.com>
2025-12-12 18:00:32 +0800

    分离节点

--- __init__.py
+++ __init__.py
@@ -1,15 +1,11 @@
-from .node import RN_Translator_Node, RN_Prompt_Translator, RN_SeedXPro_Translator
+from .node import RN_Translator_Node
 
 NODE_CLASS_MAPPINGS = {
     "RN_Translator_Node": RN_Translator_Node,
-    "RN Prompt Translator": RN_Prompt_Translator,
-    "RN SeedXPro Translator": RN_SeedXPro_Translator,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "RN_Translator_Node": "RunNode 翻译器",
-    "RN Prompt Translator": "RunNode Translater",
-    "RN SeedXPro Translator": "RunNode SeedXPro Translater",
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
